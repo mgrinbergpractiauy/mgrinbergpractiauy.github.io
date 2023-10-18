86e109cc8c835d4e265b5f8f13bb4ce236b717bc
Matias Grinberg <mgrinberg@matriz.com.uy>
2023-10-18 19:29:52 -0300

    pruebas

--- serviceWorker.js
+++ serviceWorker.js
@@ -6,22 +6,39 @@ const urlsToCache = [
 self.addEventListener("install", function(event) {
 // Perform install steps
 event.waitUntil(
-caches.open(CACHE_NAME)
-.then(function(cache) {
-console.log("Opened cache");
-return cache.addAll(urlsToCache);
-})
+    caches.open(CACHE_NAME).then(
+        function(cache) {
+            console.log("Opened cache");
+            return cache.addAll(urlsToCache);
+        }
+    )
 );
 self.skipWaiting();
 });
 
 self.addEventListener("fetch", function(event) {
-    event.respondWith(caches.match(event.request)
-    .then(function(response) {
-    if (response) {
-    return response;
-    }
-    return fetch(event.request);
-    })
+    event.respondWith(caches.match(event.request).then(function(response) {
+        if (response) {
+            return response;
+        }
+        return fetch(event.request);
+        }
+        )
     );
-    });
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.filter(function(cacheName) {
+            // Return true if you want to remove this cache,
+            // but remember that caches are shared across
+            // the whole origin
+          }).map(function(cacheName) {
+            return caches.delete(cacheName);
+          })
+        );
+      })
+    );
+  });
--- src/routes/PageRoutes.jsx
+++ src/routes/PageRoutes.jsx
@@ -2,27 +2,24 @@ import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { Spinner } from "../components/layout/spinners/Spinner";
-//const {HomePage} = lazy(() => import("../components/latu/Home"));
-import HomePage from "../components/latu/Home"
-import BarcodeScanner from "../components/latu/BarCode"
-import SignatureCapture from "../components/latu/Signature"
-import GeoLocalization from "../components/latu/geo/GeoLocalization"
+import HomePage from "../components/latu/Home";
+import BarcodeScanner from "../components/latu/BarCode";
+import SignatureCapture from "../components/latu/Signature";
+import GeoLocalization from "../components/latu/geo/GeoLocalization";
 
 const PageRoutes = () => {
   return (
-    <div>      
+    <div>
       <Suspense fallback={<Spinner />}>
         <BrowserRouter>
-                <Routes>
-                    <Route path='/home/:token/:user' element={<HomePage />}></Route> 
-                    <Route path='/BarCode/' element={<BarcodeScanner />}></Route>  
-                    <Route path='/Signature/' element={<SignatureCapture />}></Route> 
-                    <Route path='/Geo/' element={<GeoLocalization />}></Route> 
-                                               
-                </Routes>
-        </BrowserRouter>      
+          <Routes>
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/BarCode" element={<BarcodeScanner />} />
+            <Route path="/Signature" element={<SignatureCapture />} />
+            <Route path="/Geo" element={<GeoLocalization />} />
+          </Routes>
+        </BrowserRouter>
       </Suspense>
-   
     </div>
   );
 };
